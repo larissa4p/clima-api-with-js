89b78f37e2e522a8be568c500dcdf9f011b1d479
Larissa <98964013+larissa4p@users.noreply.github.com>
2022-09-23 15:35:36 -0300

    api

--- js/scripts.js
+++ js/scripts.js
@@ -10,21 +10,33 @@ const tempElement = document.querySelector("#temperature span");
 const descElement = document.querySelector("#description");
 const weatherIconElement = document.querySelector("#weather-icon");
 const countryElement = document.querySelector("#country");
-const umidityElement = document.querySelector("#umidity span");
+const humidityElement = document.querySelector("#humidity span");
 const windElement = document.querySelector("#wind span");
 
 // Funções
 const getWeatherData = async (city) => {
   const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`;
+
   const res = await fetch(apiWeatherURL);
   const data = await res.json();
 
-  console.log(data);
+  return(data);
 };
 
-const showWeatherData = (city) => {
-  getWeatherData(city);
-};
+const showWeatherData = async(city) => {
+  const data = await getWeatherData(city);
+
+  cityElement.innerText = data.name
+  tempElement.innerText = parseInt(data.main.temp)
+  descElement.innerText = data.weather[0].description
+  weatherIconElement.setAttribute(
+    "src",
+    `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`
+  );
+  countryElement.setAttribute("src", apiCountryURL + data.sys.country);
+  humidityElement.innerHTML = `${data.main.humidity}%`;
+  windElement.innerHTML = `${data.wind.speed}km/h`
+}
 
 // Eventos
 searchBtn.addEventListener("click", (e) => {
